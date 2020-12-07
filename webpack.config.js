646f57b11cfc79c0f5640dc93ab19470cabbe79c
Mårten Frisk <marten.frisk@askas.se>
2020-12-07 08:59:43 +0100

    Add webpackconfig to fix fauna bug

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: [
+		new webpack.DefinePlugin({ 'global.GENTLY': false })
+	],
+	node: {
+		__dirname: true,
+	},
+	alias: {
+		'inherits': 'inherits/inherits_browser.js',
+		'superagent': 'superagent/lib/client',
+		'emitter': 'component-emitter',
+	},	
+}
