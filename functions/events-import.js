cd01137f16f29af0a459f707cd0e76c664896482
Mårten Frisk <marten.frisk@askas.se>
2020-12-07 08:52:10 +0100

    Add test function and netlify config

--- functions/events-import.js
+++ functions/events-import.js
@@ -1,4 +1,5 @@
 import faunadb from 'faunadb'
+import fetch from 'node-fetch'
 
 /* 
 https://www.netlify.com/blog/2018/07/09/building-serverless-crud-apps-with-netlify-functions-faunadb/
@@ -8,9 +9,29 @@ https://css-tricks.com/lets-build-a-jamstack-e-commerce-store-with-netlify-funct
 
 const q = faunadb.query
 const client = new faunadb.Client({
-	secret: process.env.FAUNADB_SECRET
+	secret: process.env.NEXT_FAUNA_ADMIN_SECRET
 })
 
-exports.handler = (event, context, callback) => {
+const url = 'https://www.thesportsdb.com/api/v1/json/1/eventsnextleague.php?id=4328'
 
+exports.handler = (event, context) => {
+	const data = fetch(url)
+		.then(res => res.json())
+		.catch(error => ({ statusCode: 422, body: String(error)}))
+	data.events.map(event => (
+		client.query(q.Create(q.Collection('Events'), { 
+			data: {
+				homeTeamName: event.strHomeTeam,
+				awayTeamName: event.strAwayTeam,
+				homeTeamId: event.idHomeTeam,
+				awayTeamId: event.idAwayTeam,
+				dateTime: event.strTimestamp,
+				venue: event.strVenue,
+				divisionName: event.strLeague,
+				divisionId: event.idLeague,
+			}
+		}))
+			.then(res => console.log(res))
+			.catch(err => console.error('Error: %s', err))
+	))
 }
