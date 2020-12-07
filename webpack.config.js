37b33e30f59b047c93c5e9e094f699e5ca32c8ae
Mårten Frisk <marten.frisk@askas.se>
2020-12-07 09:04:00 +0100

    Add webpackconfig to fix fauna bug

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const webpack = require('webpack')
+
 module.exports = {
-	plugins: [
-		new webpack.DefinePlugin({ 'global.GENTLY': false })
-	],
-	node: {
-		__dirname: true,
-	},
-	alias: {
-		'inherits': 'inherits/inherits_browser.js',
-		'superagent': 'superagent/lib/client',
-		'emitter': 'component-emitter',
-	},	
+	plugins: [ new webpack.DefinePlugin({ 'global.GENTLY': false }) ]
 }
