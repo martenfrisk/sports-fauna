d57f75d7be5ea4bb4d233e7246d3dd7cb3628bd4
Mårten Frisk <marten.frisk@askas.se>
2020-12-10 13:58:35 +0100

    Move enum converters to separate file

---
+++ utils/converters.tsx
@@ -0,0 +1,16 @@
+
+export const convertToEnum = (valueInput: string) => {
+	if (valueInput === '1') return 'HOMEWIN'
+	if (valueInput === 'X') return 'DRAW'
+	if (valueInput === '2') return 'AWAYWIN'
+}
+export const convertEnum = (enumInput: string) => {
+	if (enumInput === 'HOMEWIN') return '1'
+	if (enumInput === 'DRAW') return 'X'
+	if (enumInput === 'AWAYWIN') return '2'
+}
+export const convertEnumToTeamName = (enumInput: string, away: string, home: string) => {
+	if (enumInput === 'HOMEWIN') return away
+	if (enumInput === 'DRAW') return 'Draw'
+	if (enumInput === 'AWAYWIN') return home
+}
