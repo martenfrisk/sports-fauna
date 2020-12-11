2535db1b197b40548b4f8a8ee82ef688e72e0abd
Mårten Frisk <marten.frisk@askas.se>
2020-12-11 16:48:38 +0100

    Edit guess page with my guesses

--- components/guesses/my-guesses.tsx
+++ components/guesses/my-guesses.tsx
@@ -2,10 +2,10 @@ import Image from 'next/image'
 import { convertEnumToTeamName } from '@/utils/converters'
 import { UserGuess } from '@/utils/types'
 
-const MyGuesses = (guess: UserGuess, key: string) => (
-	<div className="flex w-full" key={key}>
-		<div className="flex items-center justify-center w-1/2">
-			<Image src={guess.eventId.homeTeamId.badge} height={30} width={30} />
+const MyGuesses = (guess: UserGuess) => (
+	<div className="flex w-full mb-3">
+		<div className="flex items-center justify-start w-1/2">
+			<Image src={guess.eventId.homeTeamId.badge} height={25} width={25} />
 			<span className="ml-4">
 				{guess.eventId.homeTeamName}
 			</span>
@@ -13,14 +13,23 @@ const MyGuesses = (guess: UserGuess, key: string) => (
 			<span className="mr-4">
 				{guess.eventId.awayTeamName}
 			</span>
-			<Image src={guess.eventId.awayTeamId.badge} height={30} width={30} />
+			<Image src={guess.eventId.awayTeamId.badge} height={25} width={25} />
 
 		</div>
 		<div className="flex items-center w-1/2 space-x-4">
 			<span>
-									Winner:&nbsp;
-				{guess.winner && convertEnumToTeamName(guess.winner, guess.eventId.awayTeamName, guess.eventId.homeTeamName)}
+				{guess.winner === 'DRAW' ? (
+					'Draw'
+				): (
+					<span>
+						{guess.winner && (
+							convertEnumToTeamName(guess.winner, guess.eventId.awayTeamName, guess.eventId.homeTeamName)
+						)}
+						&nbsp;to win
+					</span>
+				)}
 			</span>
+			{/* <span onClick={() => handleRemove(guess._id)}>Delete</span> */}
 		</div>
 	</div>
 )
--- pages/guess/[slug].tsx
+++ pages/guess/[slug].tsx
@@ -1,29 +1,37 @@
-import { useState, useContext } from 'react'
+import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import { getAuthCookie, getUserCookie } from '@/utils/auth-cookies'
-import { FindLeagueBySlug, FindUserGuessByID } from '@/utils/graphql-requests'
-import { League, UserGuess, TeamType } from '@/utils/types'
+import { createNewUserGuess, FindLeagueBySlug, FindUserGuessByID, RemoveGuess } from '@/utils/graphql-requests'
+import { League, UserGuess, TeamType, Event } from '@/utils/types'
 import Layout from '@/components/layout'
-import { UserContext } from '@/utils/user-context'
 import MyGuesses from '@/components/guesses/my-guesses'
 import { convertEnum, convertToEnum } from '@/utils/converters'
+import { useRouter } from 'next/router'
 
 
-const Guess = ({league, myGuesses}: {league: League, myGuesses: [UserGuess]}) => {
+const Guess = ({league, myGuesses, token, userID}: {league: League, myGuesses: [UserGuess], token: string, userID: string}) => {
+	const router = useRouter()
 
 	const [unsaved, setUnsaved] = useState(false)
 	const [newWinnerGuess, setNewWinnerGuess] = useState([])
-	const [newScoreGuess, setNewScoreGuess] = useState([])
+	
+	const [guessesFromDb, setGuessesFromDb] = useState([])
+	const filterOwn = myGuesses.filter(x => x.user._id === userID)
+	useEffect(() => {
+		setGuessesFromDb(() => filterOwn)
+	}, [])
 	const handleInputChange = ({target}, event: any, guessType: string) => {
 		const { value } = target
 		setUnsaved(true)
 		const inputGuess = { 
 			eventId: event._id,
+			apiEventId: event.eventId,
 			homeTeam: event.homeTeamName,
 			awayTeam: event.awayTeamName,
 			guess: value,
 			withEnum: convertToEnum(value),
-			type: guessType
+			type: guessType,
+			...event
 		}
 		if (guessType === 'winner') {
 			if (newWinnerGuess.some(x => x.eventId === event._id)) {
@@ -31,18 +39,38 @@ const Guess = ({league, myGuesses}: {league: League, myGuesses: [UserGuess]}) =>
 			} else {
 				setNewWinnerGuess(() => [...newWinnerGuess, inputGuess])
 			}
-		} else {
-			if (newScoreGuess.some(x => x.eventId === event._id)) {
-				setNewScoreGuess(() => [
-					newScoreGuess.filter(x => x.eventId !== event._id),
-					inputGuess
-				])
-			} else {
-				setNewScoreGuess(() => [...newScoreGuess, inputGuess])
-			}
-		}
+		} 
 	}
-	const { userID } = useContext(UserContext)
+
+	const handleRemoveGuess = async (guessId: string) => {
+		RemoveGuess(token, guessId)
+		const filteredGuess = guessesFromDb.filter(x => x._id !== guessId)
+		setGuessesFromDb(() => filteredGuess)
+		const { slug } = router.query
+		router.push(`/guess/${slug}`)
+		// await FindUserGuessByID(token)
+		// 	.then((data) => setGuessesFromDb(() => data.allGuesses.data))
+	}
+
+	// useEffect(() => {
+	// 	setGuessesFromDb(myGuesses)
+	// 	console.log('new import')
+	// }, [unsaved])
+
+	const handleSave = async () => {
+		newWinnerGuess.map(guess => {
+			createNewUserGuess(token, userID, guess.eventId, guess.apiEventId, guess.withEnum)
+			console.log('guess created')
+		})
+		setNewWinnerGuess(() => [])
+		const { slug } = router.query
+		FindUserGuessByID(token)
+			.then((data) => setGuessesFromDb(() => data.allGuesses.data))
+		router.push(`/guess/${slug}`)
+		console.log(guessesFromDb)
+		
+		setUnsaved(false)
+	}
 
 	return (
 		<Layout>
@@ -50,18 +78,20 @@ const Guess = ({league, myGuesses}: {league: League, myGuesses: [UserGuess]}) =>
 				<div className="mb-4 text-2xl">
 					{league.name}
 				</div>
-				<p className="w-full text-center">
-					{myGuesses.length > 0 ? (
-						`You have ${myGuesses.length} ${myGuesses.length === 1 ? 'guess' : 'guesses'}`
+				{/* <p className="w-full mb-4 text-center">
+					{guessesFromDb.length > 0 ? (
+						`You have ${guessesFromDb.length} ${guessesFromDb.length === 1 ? 'guess' : 'guesses'}`
 					) : (
 						'You have not guessed any games yet.'
 					)}
 				</p>
 				<div className="flex flex-col items-center w-full">
-					{myGuesses.map((guess: UserGuess) => (
-						<MyGuesses {...guess} key={guess._id}/>
+					{guessesFromDb.map((guess: UserGuess) => (
+						<div className="flex w-full" key={guess._id}>
+							<MyGuesses {...guess} />
+						</div>
 					))}
-				</div>
+				</div> */}
 				<div key={league.slug} className="w-full max-w-2xl mt-10">
 					
 					{league.options.teams.map(
@@ -87,65 +117,42 @@ const Guess = ({league, myGuesses}: {league: League, myGuesses: [UserGuess]}) =>
 										</div>
 										<div className="w-2/3">
 											{mergedGames.map(
-												(event: any) => (
+												(event: Event) => (
 													<div className="p-2 mb-4 rounded-md bg-gray-50" key={event.dateTime}>
-														<div className="flex justify-between w-full mb-2">
-															<span className="w-1/4 text-sm">
+														<div className="flex flex-wrap justify-between w-full mb-2">
+															<span className="w-full -mb-4 text-sm text-left">
 																{new Date(event.dateTime).toLocaleDateString()}
 															</span>
-															<div className="flex justify-between w-3/4">
-																<span className={`text-base ${team.teamName === event.homeTeamName && 'font-semibold'}`}>
+															<div className="flex justify-center w-full">
+																<span className={`text-base w-1/2 text-right mr-2 ${team.teamName === event.homeTeamName && 'font-semibold'}`}>
 																	{event.homeTeamName}
 																</span>
-																<span className={`text-base ${team.teamName === event.awayTeamName && 'font-semibold'}`}>
+																<span className={`text-base w-1/2 ml-2 ${team.teamName === event.awayTeamName && 'font-semibold'}`}>
 																	{event.awayTeamName}
 																</span>
 															</div>
 														</div>
 														<div className="flex items-center w-full">
-															{userID && event.submittedGuesses.data.find((x) => x.user._id === userID.id) ? (
-																<>
-																	<div className="w-1/4 p-1 text-xs bg-green-100 rounded-sm">
+															{userID && event.submittedGuesses.data.find((x) => x.user._id === userID) ? (
+																<div className="flex justify-between w-full p-1 text-xs bg-green-100 rounded-md">
+																	<span className="w-1/5"></span>
+																	<div className="flex justify-center w-3/5">
 																		<span className="mr-1 text-xs">
-																				Your guess:
+																					Your guess:
 																		</span>
 																		<span className="font-semibold">
-																			{convertEnum(event.submittedGuesses.data.find((x) => x.user._id === userID.id).winner)}
+																			{convertEnum(event.submittedGuesses.data.find((x) => x.user._id === userID).winner)}
 																		</span>
 																	</div>
-																	<div className="flex justify-center w-full">
-																		<label className="inline-flex items-center">
-																			<input
-																				name={event._id}
-																				type="radio"
-																				value="1"
-																				onChange={(e) => handleInputChange(e, event, 'winner')}
-																			/>
-																			<span className="ml-2">1</span>
-																		</label>
-																		<label className="inline-flex items-center mx-2">
-																			<input
-																				name={event._id}
-																				type="radio"
-																				value="X"
-																				onChange={(e) => handleInputChange(e, event, 'winner')}
-																			/>
-																			<span className="ml-2">X</span>
-																		</label>
-																		<label className="inline-flex items-center">
-																			<input
-																				name={event._id}
-																				type="radio"
-																				value="2"
-																				onChange={(e) => handleInputChange(e, event, 'winner')}
-																			/>
-																			<span className="ml-2">2</span>
-																		</label>
-																	</div>
-																</>
+																	<span
+																		className="w-1/5 pr-2 text-right cursor-pointer hover:underline font-xs"
+																		onClick={() => handleRemoveGuess(event.submittedGuesses.data.find((x) => x.user._id === userID)._id)}
+																	>
+																		delete
+																	</span>
+																</div>
 															) : (
 																<>
-																	<div className="w-1/4 text-sm"></div>
 																	<div className="flex justify-center w-full">
 																		<label className="inline-flex items-center">
 																			<input
@@ -193,24 +200,13 @@ const Guess = ({league, myGuesses}: {league: League, myGuesses: [UserGuess]}) =>
 
 				{unsaved &&
 				<div className="fixed bottom-0 flex flex-col items-center px-10 py-2 bg-white rounded-md bg-opacity-90 ">
-					{/* <div className="relative self-end -mt-1">
-						<button onClick={() => setUnsaved(false)} className="mb-2 -mr-6 text-lg">x</button>
-					</div> */}
-					<div className="flex items-center">
-						<p className="w-1/2 mb-2 -mt-2 text-base">
-							You have unsaved guesses
-						</p>
-						{newWinnerGuess.length > 0 && (
-							<div className="flex flex-col w-1/2 ml-2 text-sm">
-								{newWinnerGuess.map((guess) => (
-									<p key={guess.eventId}>
-										{guess.homeTeam} vs {guess.awayTeam}: {guess.guess}
-									</p>
-								))}
-							</div>
-						)}
-					</div>
-					<button className="px-3 py-1 text-white rounded-md bg-gradient-to-b from-blue-500 to-blue-700">Save</button>
+							You have {newWinnerGuess.length} unsaved {newWinnerGuess.length === 1 ? 'guess' : 'guesses'}
+					<button
+						className="px-3 py-1 text-white rounded-md bg-gradient-to-b from-blue-500 to-blue-700"
+						onClick={handleSave}
+					>
+							Save
+					</button>
 				</div>
 				}
 			</div>
@@ -222,15 +218,16 @@ export const getServerSideProps = async (ctx: any) => {
 	const token = getAuthCookie(ctx.req)
 	const userID = getUserCookie(ctx.req)
 	const { slug } = ctx.params
-
-	const myGuesses = await FindUserGuessByID(token, userID)
+	const myGuesses = await FindUserGuessByID(token)
 
 	const data = await FindLeagueBySlug(token, slug)
 
 	return {
 		props: {
+			token: token || null,
+			userID: userID || null,
 			league: data?.findLeague.data[0],
-			myGuesses: myGuesses?.findGuessByUserId.data
+			myGuesses: myGuesses?.allGuesses.data || null
 		}
 	}
 }
