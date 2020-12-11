b225e9958dc242727853c13a6aca300b86aa75a3
Mårten Frisk <marten.frisk@askas.se>
2020-12-11 16:48:08 +0100

    Add file with fauna functions for reference

---
+++ utils/extra-functions.js
@@ -0,0 +1,35 @@
+const findLeague = `Query(
+  Lambda(
+    ["slug", "size", "after", "before"],
+    Let(
+      {
+        match: Filter(
+          Match(Index("allLeagues")),
+          Lambda(
+            "ref",
+            Let(
+              { League: Get(Var("ref")) },
+              And(
+                If(
+                  IsNull(Var("slug")),
+                  true,
+                  Equals(Select(["data", "slug"], Var("League")), Var("slug"))
+                )
+              )
+            )
+          )
+        ),
+        page: If(
+          Equals(Var("before"), null),
+          If(
+            Equals(Var("after"), null),
+            Paginate(Var("match"), { size: Var("size") }),
+            Paginate(Var("match"), { after: Var("after"), size: Var("size") })
+          ),
+          Paginate(Var("match"), { before: Var("before"), size: Var("size") })
+        )
+      },
+      Map(Var("page"), Lambda("ref", Get(Var("ref"))))
+    )
+  )
+)`
