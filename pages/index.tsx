ccff747b7f65eae982de9ceeb49bcdaf5bee73d5
Mårten Frisk <marten.frisk@askas.se>
2020-12-15 10:15:22 +0100

    Add svg images to landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -29,41 +29,46 @@ const Home = ({token}: { token: any}) => {
 				)	 : (
 					<div className="flex flex-wrap w-full">
 						<div className="flex flex-col items-center justify-center w-full md:w-1/2">
-							<div className="text-2xl leading-loose text-center text-blue-700">
+							<div className="text-xl leading-loose text-center text-blue-700 md:text-4xl">
 								<p>Guess football results</p>
 								<p>Compete with your friends</p>
 							</div>
 							<Link href="/signup">
-								<a className="px-4 py-px my-2 text-lg font-semibold text-white border-2 border-blue-500 rounded-md shadow-xl bg-gradient-to-br from-blue-600 to-blue-300">Join</a>
+								<a className="px-4 py-px mt-4 mb-12 text-lg font-semibold text-white border-2 border-blue-500 rounded-md shadow-xl bg-gradient-to-br from-blue-600 to-blue-300">Join</a>
 							</Link>
 
-							<div className="px-12 py-2 text-lg font-light text-blue-700 bg-white rounded-md shadow-2xl">
+							<div className="px-6 py-4 text-lg font-normal text-blue-700 bg-white rounded-md shadow-blue-lg">
 								<div className="flex items-center w-full">
 									<div className="w-1/2">
-										<p>1.&nbsp;Create&nbsp;a&nbsp;league</p>
-										<p className="mt-2 ml-2">2.&nbsp;Choose&nbsp;teams</p>
+										<p className="">1.&nbsp;Create&nbsp;a&nbsp;league</p>
+										<p className="mt-2 ">2.&nbsp;Choose&nbsp;teams</p>
 									</div>
-									<div className="w-1/2">
+									<div className="w-32 opacity-50">
 										<Trophy />
 									</div>
 								</div>
 								<div className="flex items-center">
-									<div className="w-1/2">
+									<div className="w-32 opacity-50">
 										<Training />
 									</div>
 									<div className="w-1/2">
-										<p>3.&nbsp;Invite&nbsp;friends</p>
-										<p className="mt-2 ml-2">4.&nbsp;Start&nbsp;guessing!</p>
+										<p className="">3.&nbsp;Invite&nbsp;friends</p>
+										<p className="mt-2 ">4.&nbsp;Start&nbsp;guessing!</p>
 									</div>
 								</div>
 							</div>
 						</div>
-						<div className="flex items-center justify-center w-full md:w-1/2">
+						<div className="flex items-center justify-center w-full pt-6 overflow-visible md:w-1/2">
 							<Illustration />
 						</div>
 					</div>
 				)}
 			</div>
+			<style jsx>{`
+				.dropshadow {
+					filter: drop-shadow(-3px 3px 2px rgba(29, 78, 216, 0.2));
+				}
+			`}</style>
 		</Layout>
 	)
 }
