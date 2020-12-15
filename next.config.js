fb07f8c4e9b0be00049283b473178bd3f7fe53d8
Mårten Frisk <marten.frisk@askas.se>
2020-12-15 15:48:45 +0100

    Add PWA support using next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+module.exports = withPWA({
+	pwa: {
+		dest: 'public'
+	},
 	target: 'serverless',
 	images: {
 		domains: ['thesportsdb.com', 'www.thesportsdb.com']
 	}
-}
+})
