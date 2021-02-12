fd8546372ae6352d64a8666adf2124bd561fbc9c
Marten <marten.frisk@gmail.com>
2021-02-12 20:10:06 +0100

    Minor fixes to fit with firebase

--- pages/league/[slug].tsx
+++ pages/league/[slug].tsx
@@ -1,85 +1,86 @@
 import Layout from '@/components/layout'
-import { getAuthCookie } from '@/utils/auth-cookies'
 import TeamPicker from '@/components/teampicker'
+import EventItem from '@/components/event'
 import { useEffect, useState } from 'react'
 import LeagueOptions from '@/components/league-options'
 import LeagueStandings from '@/components/league-standings'
-import { FindLeague, getAllTeamTypes, getEventsFromDb, updateTeam } from '@/utils/graphql-requests'
-import { League as LeagueType, TeamType } from '@/utils/types'
+import {
+	FindLeague,
+	getAllTeams,
+	getEventsFromDb,
+	updateLeagueOptions,
+} from '@/utils/firebase-requests'
 import Image from 'next/image'
 import Link from 'next/link'
 
-const SavePopup = ({saveChanges, unsavedChanges, updateMessage}: {saveChanges: any, unsavedChanges: boolean, updateMessage: any}) => {
-	return (<div className="flex flex-col items-center w-full my-6">
-		<button onClick={() => saveChanges()} className={`px-6 py-2 text-lg border ${unsavedChanges ? 'border-red-600 text-red-600' : 'border-blue-600'} rounded-md`}>
-			{unsavedChanges ? 'You have unsaved changes. Click to save.' : 'Save changes'}
-		</button>
-		{updateMessage && <p className="absolute mt-12 text-sm">{updateMessage}</p>}
-	</div>)
-}
-
-const teamEvent: (value: any, index: number, array: any[]) => JSX.Element = (team) => {
-	const mergedGames  = [...team.events.homeEvents.data, ...team.events.awayEvents.data]
-	const filteredGames = mergedGames
-		.filter(game => new Date(game.dateTime) > new Date() && game)
-		.sort((a, b) => b.dateTime - a.dateTime)
+const SavePopup = ({
+	saveChanges,
+	unsavedChanges,
+	updateMessage,
+}: {
+  saveChanges: any
+  unsavedChanges: boolean
+  updateMessage: any
+}) => {
 	return (
-		<div key={team.name} className="flex flex-wrap items-start w-full mt-4">
-			<div className="w-1/5 text-lg text-gray-800">
-				<Image src={team.events.badge} alt={`${team.name} badge`} height={30} width={30} />
-			</div>
-			<div className="w-4/5">
-				{filteredGames.slice(0, 2).map((event) => (
-					<>
-						<p>
-							<span className={`${event.homeTeamName === team.name && 'font-semibold'} mr-1`}>
-								{event.homeTeamName}
-							</span>
-							<span className="text-sm">
-								vs
-							</span>
-							<span className={`${event.awayTeamName === team.name && 'font-semibold'} ml-1`}>
-								{event.awayTeamName}
-							</span>
-						</p>
-						<p className="text-xs text-gray-800">{new Date(
-							event.dateTime)
-							.toLocaleDateString('sv-SE')}</p>
-					</>
-				))}
-			</div>
+		<div className="flex flex-col items-center w-full my-6">
+			<button
+				onClick={() => saveChanges()}
+				className={`px-6 py-2 text-lg border ${
+					unsavedChanges ? 'border-red-600 text-red-600' : 'border-blue-600'
+				} rounded-md`}
+			>
+				{unsavedChanges
+					? 'You have unsaved changes. Click to save.'
+					: 'Save changes'}
+			</button>
+			{updateMessage && (
+				<p className="absolute mt-12 text-sm">{updateMessage}</p>
+			)}
 		</div>
 	)
 }
-const League = ({ data, teams, token }: { data: LeagueType, teams: [TeamType], token: any }) => {
-	const [pickedTeam, setPickedTeam] = useState(
-		data.options.teams
-	)
-	const [options, setOptions] = useState(
-		data.options
-	)
+
+const League = ({ data, teams }: { data: any; teams: any }) => {
+	const [pickedTeam, setPickedTeam] = useState<any>(Object.values(data.teams))
+	const [isPublic, setIsPublic] = useState<boolean>(data.public)
 	const [unsavedChanges, setUnsavedChanges] = useState(false)
 	const [upcomingEvents, setUpcomingEvents] = useState([])
 	const [errorMessage, setErrorMessage] = useState('')
 	const [updateMessage, setUpdateMessage] = useState('')
 
 	const saveChanges = () => {
-		updateTeam(token, options, pickedTeam, setUpdateMessage, setErrorMessage)
+		updateLeagueOptions(
+			data.slug,
+			isPublic,
+			pickedTeam,
+			setUpdateMessage,
+			setErrorMessage
+		)
 		setUnsavedChanges(false)
 	}
 
 	useEffect(() => {
-		if (options !== data.options || pickedTeam !== data.options.teams) {
+		if (
+			isPublic !== data.public ||
+      pickedTeam.length !== Object.values(data.teams).length
+		) {
 			setUnsavedChanges(true)
+			setUpdateMessage('')
 		}
-	}, [options, pickedTeam])
-	
+	}, [isPublic, pickedTeam])
+
 	useEffect(() => {
 		setUpcomingEvents([])
-		pickedTeam.map((team) => {
-			getEventsFromDb(token, team, setUpcomingEvents)
-		}
-		)
+		pickedTeam.map(async (team: any) => {
+			const events = await getEventsFromDb('2021', team.id)
+			if (events) {
+				setUpcomingEvents((prev) => [
+					...prev,
+					{ team: team.name, crestUrl: team.crestUrl, events: events },
+				])
+			}
+		})
 	}, [pickedTeam])
 
 	return (
@@ -88,23 +89,31 @@ const League = ({ data, teams, token }: { data: LeagueType, teams: [TeamType], t
 			{data && (
 				<div className="flex justify-center w-full">
 					<Link href={`/guess/${data.slug}`}>
-						<a className="btn-blue">
-							Start guessing
-						</a>
+						<a className="btn-blue">Start guessing</a>
 					</Link>
 				</div>
 			)}
 			<div className="flex flex-wrap w-full mt-4">
 				<div className="flex flex-wrap items-start order-2 w-full p-4 bg-blue-100 rounded-md sm:order-1 sm:w-1/2">
-					<div className="w-full">
-						Upcoming games
-					</div>
+					<div className="w-full mb-2">Upcoming games</div>
 					{upcomingEvents !== [] ? (
-						upcomingEvents.map(teamEvent)
+						upcomingEvents.map((event) => (
+							<div className="w-full mb-4" key={event.team}>
+								<p className="flex items-center w-full mb-2">
+									{event.crestUrl && (
+										<Image src={event.crestUrl} width={30} height={30} />
+									)}
+									<span className="ml-4 text-lg">
+										{event.team}
+									</span>
+								</p>
+								<EventItem event={event.events} team={event.team} />
+							</div>
+							// <pre>{JSON.stringify(event, null, 2)}</pre>
+						))
 					) : (
 						<div>Loading...</div>
-					)
-					}
+					)}
 				</div>
 				{data ? (
 					<div className="flex flex-col items-center order-1 w-full p-4 mb-4 sm:items-start sm:order-2 sm:w-1/2">
@@ -113,75 +122,55 @@ const League = ({ data, teams, token }: { data: LeagueType, teams: [TeamType], t
 							<div className="w-1/2 text-left">{data.name}</div>
 						</div>
 						<div className="flex justify-between w-full">
-							<div className="w-1/2">
-								Members: 
-							</div>
+							<div className="w-1/2">Members:</div>
 							<div className="flex flex-col w-1/2">
-								{data.members && data.members.data.length > 0 ? (
-									data.members.data.map((item) => (
-										<div key={item.username}>
-											{item.username}
-										</div>
+								{data.members ? (
+									Object.values(data.members).map((user: any) => (
+										<span key={user.username}>{user.username}</span>
 									))
 								) : (
-									<span className="ml-2 text-sm italics">
-								No members :(
-									</span>
+									<span className="ml-2 text-sm italics">No members :(</span>
 								)}
 							</div>
 						</div>
 						<div className="mt-4">
-							<p className="text-sm text-center">
-								League options
-							</p>
-							<LeagueOptions optionsData={[options, setOptions]} />
+							<p className="text-sm text-center">League options</p>
+							<LeagueOptions optionsData={[isPublic, setIsPublic]} />
 						</div>
-						{data.standings && (
-							<LeagueStandings data={data.standings.data} />
-						)}
-
+						{data.standings && <LeagueStandings data={data.standings.data} />}
 					</div>
-				): (
+				) : (
 					<div>Loading...</div>
 				)}
-				<div>
-
-				</div>
+				<div></div>
 			</div>
-			
-			<SavePopup 
+
+			<SavePopup
 				saveChanges={saveChanges}
 				unsavedChanges={unsavedChanges}
 				updateMessage={updateMessage}
 			/>
-			
-			{errorMessage && (
-				<p>{errorMessage}</p>
-			)}
+
+			{errorMessage && <p>{errorMessage}</p>}
 			<div className="flex justify-center w-full">
 				<TeamPicker teams={teams} picker={[pickedTeam, setPickedTeam]} />
 			</div>
-
 		</Layout>
 	)
 }
 
-
 export async function getServerSideProps(ctx: any) {
 	const { slug } = ctx.params
-	const token = getAuthCookie(ctx.req)
-	const teams = await getAllTeamTypes(token)
-	const data = await FindLeague(token, slug)
-
-	return { 
-		props: { 
-			token: token || null,
-			data: data?.findLeague.data[0],
-			teams: teams?.allTeams.data
-		} 
+	// const token = getAuthCookie(ctx.req)
+	const teams = await getAllTeams('2021')
+	const data = await FindLeague(slug)
+	return {
+		props: {
+			// token: token || null,
+			data: data || null,
+			teams: teams || null,
+		},
 	}
 }
-
-    
 
 export default League
