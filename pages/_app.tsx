d04f8f19f787afce9234e588ba39057f6dee7e5b
Marten <marten.frisk@gmail.com>
2021-02-12 19:57:59 +0100

    Replace next-firebaseauth with previous context manager

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,9 @@
 import type { AppProps } from 'next/app'
 import { UserContextProvider } from '@/utils/user-context'
 import '../styles/globals.css'
+// import initAuth from '@/utils/initAuth'
+
+// initAuth()
 
 // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 function MyApp({ Component, pageProps }: AppProps) {
