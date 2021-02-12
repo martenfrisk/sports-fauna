3a9cc7183334271056936f2f3469ce6a9b6ff728
Marten <marten.frisk@gmail.com>
2021-02-12 19:55:13 +0100

    Add more valid domains to image component

--- next.config.js
+++ next.config.js
@@ -3,10 +3,19 @@ const withPWA = require('next-pwa')
 module.exports = withPWA({
 	pwa: {
 		dest: 'public',
-		mode: 'production'
+		mode: 'production',
 	},
 	target: 'serverless',
 	images: {
-		domains: ['thesportsdb.com', 'www.thesportsdb.com']
-	}
+		domains: [
+			'thesportsdb.com',
+			'www.thesportsdb.com',
+			'media.api-sports.io',
+			'https://media.api-sports.io',
+			'https://crests.football-data.org',
+			'crests.football-data.org',
+			'https://football-data.org',
+			'football-data.org',
+		],
+	},
 })
