9357e31d204db91804fdf4b2c88f8405e2f017ec
Marten <marten.frisk@gmail.com>
2021-02-20 21:49:24 +0100

    Add fetcher and winner-converter to separate file

--- utils/extra-functions.js
+++ utils/extra-functions.js
@@ -1,35 +1,14 @@
-export const findLeague = `Query(
-  Lambda(
-    ["slug", "size", "after", "before"],
-    Let(
-      {
-        match: Filter(
-          Match(Index("allLeagues")),
-          Lambda(
-            "ref",
-            Let(
-              { League: Get(Var("ref")) },
-              And(
-                If(
-                  IsNull(Var("slug")),
-                  true,
-                  Equals(Select(["data", "slug"], Var("League")), Var("slug"))
-                )
-              )
-            )
-          )
-        ),
-        page: If(
-          Equals(Var("before"), null),
-          If(
-            Equals(Var("after"), null),
-            Paginate(Var("match"), { size: Var("size") }),
-            Paginate(Var("match"), { after: Var("after"), size: Var("size") })
-          ),
-          Paginate(Var("match"), { before: Var("before"), size: Var("size") })
-        )
-      },
-      Map(Var("page"), Lambda("ref", Get(Var("ref"))))
-    )
-  )
-)`
+export const fetcher = (url) => fetch(url).then((r) => r.json())
+
+export const convertWinner = (winner) => {
+	switch (winner) {
+	case 'HOME_TEAM':
+		return '1'
+	case 'AWAY_TEAM':
+		return '2'
+	case 'DRAW':
+		return 'X'
+	default:
+		return 'error'
+	}
+}
