95b684d8203f494e558cebdd181c4c3fd7118a32
Marten <marten.frisk@gmail.com>
2021-02-20 21:45:50 +0100

    Integrate user api with firebase

--- pages/api/user.js
+++ pages/api/user.js
@@ -1,19 +1,27 @@
-import { query as q } from 'faunadb'
-import { authClient } from '@/utils/fauna-client'
-import { getAuthCookie } from '@/utils/auth-cookies'
+import { getAuthCookie, removeAuthCookie } from '@/utils/auth-cookies'
+import { db } from '@/utils/firebase'
+import { getUserDetails } from '@/utils/serverside-requests'
 
 export default async function user(req, res) {
 	const token = getAuthCookie(req)
 
 	if (!token) {
+		removeAuthCookie(res)
 		return res.status(200).send('Not logged in')
 	}
 
 	try {
-		const { ref, data } = await authClient(token).query(q.Get(q.CurrentIdentity()))
-		res.status(200).json({ ...data, id: ref.id })
+		const data = await getUserDetails(token)
+		let username 
+		if (data) {
+			username = await db.ref(`users/${data.uid}`).once('value').then((data) => data.child('username').val())
+		}
+
+		// console.log(data)
+		res.status(200).json({ id: data.user_id, username})
 	} catch (error) {
 		console.error(error)
-		res.status(error.requestResult.statusCode).send(error.message)
+		removeAuthCookie(res)
+		res.status(404).send(error)
 	}
 }
