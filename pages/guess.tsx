0b18d11a258699313abe516ebdd5948f8ca496c7
Marten <marten.frisk@gmail.com>
2021-02-20 21:37:54 +0100

    Add firebase integration

--- pages/guess.tsx
+++ pages/guess.tsx
@@ -1,43 +1,42 @@
 import Layout from '@/components/layout'
-import { getAuthCookie, getUserCookie } from '@/utils/auth-cookies'
-import { findUserByID } from '@/utils/graphql-requests'
-import { League } from '@/utils/types/types'
+import { getUserCookie } from '@/utils/auth-cookies'
+import { getLeaguesByUser } from '@/utils/firebase-requests'
 import { NextPageContext } from 'next'
 import Link from 'next/link'
 
-const Guess = ({ data }: { data: [League]}) => {
-
-
+const Guess = ({ data }: { data: string[] }) => {
 	return (
 		<Layout>
 			<div className="flex flex-col items-center">
 				<p className="text-lg text-center">
 					Choose a league and start guessing!
 				</p>
-				{data.map((league: League) => (
-					<div key={league.slug} className="my-8">
-						<Link href={`/guess/${league.slug}`}>
-							<a className="p-4 border border-blue-400 rounded-md shadow-md hover:bg-white bg-blue-50">
-								{league.name}
-							</a>
-						</Link>
-					</div>
-				))}
+				{data &&
+					data.map((league: string) => (
+						<div key={league} className="my-8">
+							<Link href={`/guess/${league}`}>
+								<a className="p-4 border border-blue-400 rounded-md shadow-md hover:bg-white bg-blue-50">
+									{league}
+								</a>
+							</Link>
+						</div>
+					))}
 			</div>
 		</Layout>
 	)
 }
 
 export const getServerSideProps = async (ctx: NextPageContext) => {
-	const token = getAuthCookie(ctx.req)
 	const userID = getUserCookie(ctx.req)
+	console.log(userID.split('|')[0])
 
-	const data = await findUserByID(token, userID)
+	const data = await getLeaguesByUser(userID.split('|')[0])
+	console.log(data)
 
 	return {
 		props: {
-			data: data?.findUserByID.leagues.data
-		}
+			data: data || null,
+		},
 	}
 }
 
