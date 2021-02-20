e9924f4fdabf9bd0719f5c36f0a14598b96869d8
Marten <marten.frisk@gmail.com>
2021-02-20 21:32:16 +0100

    Disable PWA in dev mode

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 
 module.exports = withPWA({
 	pwa: {
 		dest: 'public',
 		mode: 'production',
+		disable: process.env.NODE_ENV !== 'production',
 	},
 	target: 'serverless',
 	images: {
