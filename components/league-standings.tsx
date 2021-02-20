88e7b3aa46266df662c8c0db756e2566fa0dabe2
Marten <marten.frisk@gmail.com>
2021-02-20 21:34:09 +0100

    Update standings component for firebase

--- components/league-standings.tsx
+++ components/league-standings.tsx
@@ -1,23 +1,27 @@
 import { Standings } from '@/utils/types/types'
 
-const LeagueStandings = ({ data }: { data: Standings[]}) => {
-	const sorted = data.sort((a, b) => {
+const LeagueStandings = ({ data }: { data: any}) => {
+	const users = []
+	Object.values(data).map((user: any) => (users.push(user)))
+	console.log(users)
+	
+	const sorted = users.sort((a, b) => {
 		if(a.points > b.points) return -1
 		if(a.points < b.points) return 1
 	})
 	return (
-		<div className="w-40 p-2 mt-4 rounded-lg bg-gray-50">
-			<p className="text-sm text-center">
+		<div className="w-1/2 px-4 py-2 mt-4 rounded-lg bg-gray-50">
+			<p className="text-sm text-left">
 				Standings
 			</p>
 			{sorted.map((position, index) => (
-				<div key={position._id} className="flex items-center">
+				<div key={position.username} className="flex items-center">
 					<span className="w-4 mr-2 text-sm">{index + 1}</span>
 					<span className="w-1/2 mr-6">
-						{position.member.username}
+						{position.username}
 					</span>
 					<span className="mr-2 text-sm">
-						{position.points}p
+						{position.points ? position.points : '0'}p
 					</span>
 				</div>
 			))}
