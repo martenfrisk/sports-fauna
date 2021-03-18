670119de68378e8ac6a97945b13de58a96e38725
Marten <marten.frisk@gmail.com>
2021-03-18 19:40:17 +0100

    Add airbnb style to eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,25 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
-	},
-	extends: [
-		'eslint:recommended',
-		'plugin:react/recommended',
-		'plugin:@typescript-eslint/recommended',
-	],
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: 12,
-		sourceType: 'module',
-	},
-	plugins: ['react', '@typescript-eslint'],
-	rules: {
-		'@typescript-eslint/explicit-module-boundary-types': 'off',
-		'@typescript-eslint/no-explicit-any': 'off',
-		'react/react-in-jsx-scope': 'off',
-		'react/jsx-filename-extension': [
-			1,
-			{ extensions: ['.js', '.jsx', '.ts', '.tsx'] },
-		],
-		indent: ['error', 'tab'],
-		'linebreak-style': ['off', 'unix'],
-		quotes: ['error', 'single'],
-		semi: ['error', 'never'],
-	},
-}
+    "env": {
+        "browser": true,
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "plugin:react/recommended",
+        "airbnb"
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 12,
+        "sourceType": "module"
+    },
+    "plugins": [
+        "react",
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
+};
