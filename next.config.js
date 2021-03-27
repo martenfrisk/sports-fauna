4d691827933fdb9942f6bf8f506cdec5c345d311
Marten <marten.frisk@gmail.com>
2021-03-27 14:30:42 +0100

    Add esbuild loader

--- next.config.js
+++ next.config.js
@@ -1,7 +1,43 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const withPWA = require('next-pwa')
+const withPWA = require('next-pwa');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
+
+function useEsbuildMinify(config, options) {
+	const terserIndex = config.optimization.minimizer.findIndex(minimizer => (minimizer.constructor.name === 'TerserPlugin'));
+	if (terserIndex > -1) {
+		config.optimization.minimizer.splice(
+			terserIndex,
+			1,
+			new ESBuildMinifyPlugin(options),
+		);
+	}
+}
+
+function useEsbuildLoader(config, options) {
+	const jsLoader = config.module.rules.find(rule => rule.test && rule.test.test('.js'));
+
+	if (jsLoader) {
+		jsLoader.use.loader = 'esbuild-loader';
+		jsLoader.use.options = options;
+	}
+}
 
 module.exports = withPWA({
+	webpack: ( config, { webpack } ) => {
+		config.plugins.push(
+			new webpack.ProvidePlugin({
+				React: 'react',
+			}),
+		);
+
+		useEsbuildMinify(config);
+		useEsbuildLoader(config, {
+			loader: 'tsx',
+			target: 'es2017',
+		});
+
+		return config;
+	},
 	pwa: {
 		dest: 'public',
 		mode: 'production',
