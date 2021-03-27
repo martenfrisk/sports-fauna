d31ea44c8e30d5110e1dc3fdff983fc55282c4bb
Marten <marten.frisk@gmail.com>
2021-03-27 14:30:21 +0100

    Remove unused styles

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,7 +3,7 @@ import Header from '@/components/header'
 
 export default function Layout({ children }: { children: any }) {
 	return (
-		<main className="grid h-screen ">
+		<main className="grid h-screen" style={{ gridTemplateRows: 'auto 1fr auto' }}>
 			<Head>
 				<title>Sport Guess - guess football results with friends</title>
 				<meta
@@ -14,18 +14,9 @@ export default function Layout({ children }: { children: any }) {
 			<Header />
 			<div className="relative z-10 w-full max-w-6xl px-6 mx-auto">
 				{children}
-
 			</div>
 			<div className="fixed bottom-0 z-0 w-screen transform -skew-y-12 bg-blue-400 opacity-50 h-2/3">
 			</div>
-			<style global jsx>{`
-				main {
-					grid-template-rows: auto 1fr auto;
-				}
-				.radialgradient {
-					clip-path: polygon(0 50%, 100% 85%, 100% 100%, 0% 100%);
-				}
-			`}</style>
 		</main>
 	)
 }
