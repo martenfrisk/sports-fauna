9b6ac8cc6ebdf5ce0a319a5aff1bf7ca7be8e180
Marten <marten.frisk@gmail.com>
2021-03-29 21:56:27 +0200

    Update nextjs

--- next.config.js
+++ next.config.js
@@ -2,17 +2,21 @@
 // const withPWA = require('next-pwa');
 
 module.exports = {
-	target: 'serverless',
-	images: {
-		domains: [
-			'thesportsdb.com',
-			'www.thesportsdb.com',
-			'media.api-sports.io',
-			'https://media.api-sports.io',
-			'https://crests.football-data.org',
-			'crests.football-data.org',
-			'https://football-data.org',
-			'football-data.org',
-		],
-	},
-}
+  future: {
+    webpack5: true,
+    strictPostcssConfiguration: true,
+  },
+  target: 'serverless',
+  images: {
+    domains: [
+      'thesportsdb.com',
+      'www.thesportsdb.com',
+      'media.api-sports.io',
+      'https://media.api-sports.io',
+      'https://crests.football-data.org',
+      'crests.football-data.org',
+      'https://football-data.org',
+      'football-data.org',
+    ],
+  },
+};
